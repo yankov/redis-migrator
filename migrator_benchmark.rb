d6fe8e5552ba48aad9fe741fbfd8a6f30d95c626
Artem Yankov <artem@badgeville.com>
2012-06-28 23:46:44 -0700

    measure populating time

--- migrator_benchmark.rb
+++ migrator_benchmark.rb
@@ -29,6 +29,7 @@ class MigratorBenchmark
   def populate_cluster(keys_num, size)
     self.loop_size = keys_num * size
     self.counter = 0
+    start_time = Time.now
 
     EM.synchrony do
       @redis = Redis::Distributed.new(@redis_hosts)
@@ -38,7 +39,13 @@ class MigratorBenchmark
         self.populate_keys(i, size)
       end
 
-      EM::add_periodic_timer( 2 ) { EM.stop if counter == loop_size }
+      EM::add_periodic_timer( 2 ) do 
+        if counter == loop_size 
+          time = Time.now - start_time
+          puts "Populating of #{keys_num} keys with #{size} members took #{time} seconds"
+          EM.stop
+        end
+      end
 
     end
   end
@@ -50,8 +57,4 @@ redis_hosts = ["redis://redis-host2.com:6379/1", "redis://redis-host3.com:6379/1
 
 mb = MigratorBenchmark.new(redis_hosts)
 
-mb.populate_cluster(1000, 100)
-
-
-
-
+mb.populate_cluster(10000, 100)
