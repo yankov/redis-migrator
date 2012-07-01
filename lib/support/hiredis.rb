04336f2f176649117fc1ed19081384e1ec9d5faf
Artem Yankov <artem@badgeville.com>
2012-06-30 20:32:06 -0700

    patch hiredis to have an id method, otherwise hashing doesn't work

--- lib/support/hiredis.rb
+++ lib/support/hiredis.rb
@@ -3,5 +3,9 @@ module EventMachine::Hiredis
     def self.connect(host = 'localhost', port = 6379, db = nil)
       new(host, port, nil, db).connect
     end
+
+    def id
+      "redis://#{@host}:#{@port}/#{@db}"
+    end
   end
 end
