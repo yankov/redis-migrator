fe19320e183c77be34743937e47cbb26408e5216
Dingding Ye <yedingding@gmail.com>
2012-12-21 11:29:32 +0800

    fix bug: to_redis_protocol should use bytesize and also zset should process one by one
    Changes from the redis 3.0.0.
    
    * The `ZRANGE`, `ZREVRANGE`, `ZRANGEBYSCORE` and `ZREVRANGEBYSCORE` commands
      now return an array containing `[String, Float]` pairs when
      `:with_scores => true` is passed.

--- lib/redis_migrator/redis_helper.rb
+++ lib/redis_migrator/redis_helper.rb
@@ -3,7 +3,7 @@ class Redis
 
     def to_redis_proto(*cmd)
       cmd.inject("*#{cmd.length}\r\n") {|acc, arg|
-        acc << "$#{arg.length}\r\n#{arg}\r\n"
+        acc << "$#{arg.to_s.bytesize}\r\n#{arg}\r\n"
       }
     end
 
@@ -23,29 +23,29 @@ class Redis
       value = redis.get(key)
       pipe << to_redis_proto("SET", key, value)
     end
-  
+
     def copy_hash(pipe, key)
       redis.hgetall(key).each do |field, value|
-        pipe << to_redis_proto("HSET", key, field, value) 
+        pipe << to_redis_proto("HSET", key, field, value)
       end
     end
-  
+
     def copy_list(pipe, key)
       redis.lrange(key, 0, -1).each do |value|
         pipe << to_redis_proto("LPUSH", key, value)
       end
     end
-  
+
     def copy_set(pipe, key)
       redis.smembers(key).each do |member|
         pipe << to_redis_proto("SADD", key, member)
       end
     end
-  
+
     def copy_zset(pipe, key)
-      redis.zrange(key, 0, -1, :with_scores => true).each_slice(2) do |member, score|
+      redis.zrange(key, 0, -1, :with_scores => true).each do |member, score|
         pipe << to_redis_proto("ZADD", key, score, member)
-      end 
+      end
     end
 
   end
