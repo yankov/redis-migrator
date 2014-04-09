d6b78bce402cc06ffd60448fe9559d7173f46c25
Gintaras Sakalauskas <gintaras.sakalauskas@gmail.com>
2014-04-09 18:03:27 +0300

    Clean up spec files

--- spec/redis_helper_spec.rb
+++ spec/redis_helper_spec.rb
@@ -1,58 +1,61 @@
 describe Redis::Helper do
+  let(:migrator) do
+    Redis::Migrator.new(['redis://localhost:6379'], ['redis://localhost:6377'])
+  end
+
+  let(:old_cluster) { migrator.old_cluster }
+  let(:new_cluster) { migrator.new_cluster }
+  let(:pipe) { PipeMock.new(new_cluster) }
+
   before do
     expect(Redis).to receive(:new).at_least(1).times do |options|
       MockRedis.new(options)
     end
 
-    @migrator = Redis::Migrator.new(["redis://localhost:6379"],
-                                    ["redis://localhost:6377"])
-
-    @r1 = @migrator.old_cluster
-    @r2 = @migrator.new_cluster
-    allow(@migrator).to receive(:redis).and_return(@r1)
-    @pipe = PipeMock.new(@r2)
+    allow(migrator).to receive(:redis).and_return(old_cluster)
   end
 
-  it "should copy a string" do
-    @r1.set("a", "some_string")
-    @migrator.copy_string(@pipe, "a")
+  it 'should copy a string' do
+    old_cluster.set('a', 'some_string')
+    migrator.copy_string(pipe, 'a')
 
-    @r2.get("a").should == "some_string"
+    new_cluster.get('a').should == 'some_string'
   end
 
-  it "should copy a hash" do
-    @r1.hmset("myhash",
-      "first_name", "James",
-      "last_name", "Randi",
-      "age", "83")
+  it 'should copy a hash' do
+    old_cluster.hmset('myhash',
+      'first_name', 'James',
+      'last_name', 'Randi',
+      'age', '83')
 
-    @migrator.copy_hash(@pipe, "myhash")
+    migrator.copy_hash(pipe, 'myhash')
 
-    @r2.hgetall("myhash").should == {"first_name" => "James", "last_name" => "Randi", "age" => "83"}
+    new_cluster.hgetall('myhash').should == {'first_name' => 'James',
+                                             'last_name' => 'Randi',
+                                             'age' => '83'}
   end
 
-  it "should copy a list" do
-    ('a'..'z').to_a.each { |val| @r1.lpush("mylist", val) }
+  it 'should copy a list' do
+    ('a'..'z').to_a.each { |val| old_cluster.lpush('mylist', val) }
+    migrator.copy_list(pipe, 'mylist')
 
-    @migrator.copy_list(@pipe, "mylist")
-
-    @r2.lrange("mylist", 0, -1).should == ('a'..'z').to_a
+    new_cluster.lrange('mylist', 0, -1).should == ('a'..'z').to_a
   end
 
-  it "should copy a set" do
-    ('a'..'z').to_a.each { |val| @r1.sadd("myset", val) }
+  it 'should copy a set' do
+    ('a'..'z').to_a.each { |val| old_cluster.sadd('myset', val) }
+    migrator.copy_set(pipe, 'myset')
 
-    @migrator.copy_set(@pipe, "myset")
-
-    @r2.smembers("myset").should == ('a'..'z').to_a
+    new_cluster.smembers('myset').should == ('a'..'z').to_a
   end
 
-  it "should copy zset" do
-    ('a'..'z').to_a.each { |val| @r1.zadd("myzset", rand(100), val) }
+  it 'should copy zset' do
+    ('a'..'z').to_a.each { |val| old_cluster.zadd('myzset', rand(100), val) }
+    migrator.copy_zset(pipe, 'myzset')
 
-    @migrator.copy_zset(@pipe, "myzset")
+    new_range = new_cluster.zrange('myzset', 0, -1, with_scores: true).sort
+    old_range = old_cluster.zrange('myzset', 0, -1, with_scores: true).sort
 
-    @r2.zrange("myzset", 0, -1, :with_scores => true).sort.should == @r1.zrange("myzset", 0, -1, :with_scores => true).sort
+    new_range.should == old_range
   end
-
 end
